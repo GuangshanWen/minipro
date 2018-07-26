73a062ff55e56d5207a45294067b369e83d7105e
ubuntu <ubuntu@localhost.localdomain>
2018-07-26 19:47:52 +0800

    add db operation and general tool

---
+++ DB.py
@@ -0,0 +1,11 @@
+
+def Insert_Into_User_DB(UserID,json_form):
+	pass
+
+def Insert_Into_Tag_Images(UserID,json_form):
+	pass
+
+def Insert_Into_Image_Tags(UserID,json_form):
+	pass
+
+
---
+++ wutil.py
@@ -0,0 +1,14 @@
+
+def Generate_UserID(json_form):
+	return json_form["nickname"]
+
+
+def Insert_Into_DB(json_form):
+	return 0
+
+def Mkdir(UserID):
+	return 0
+
+
+	
+
