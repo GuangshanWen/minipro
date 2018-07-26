4f693570ba269d4a79ceeca2cfc90c9e8f4c119a
ubuntu <ubuntu@localhost.localdomain>
2018-07-26 20:31:37 +0800

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 from flask import Flask
 from flask import request
+from wutil import *
+from DB import *
+from API import *
 
 app = Flask(__name__)
 
@@ -15,8 +18,20 @@ def upload_user_info():
 # 4. mkdir ID
 # 5. return user ID to client
 	user_info = request.get_data()
-	print user_info
-	return 'err'
+	json_form = json.loads(user_info)#1
+
+	UserID = Generate_UserID(json_form)#2
+        
+	ret = Insert_Into_User_DB(UserID,json_form)#3
+	if ret !=0 :
+		pass
+
+	ret = Mkdir(UserID)#4
+	if ret != 0 :
+		pass
+	
+	#print json_form["nickname"]
+	return UserID#5
 
 @app.route('/upload_image',methods=['POST'])
 def upload_image():
@@ -24,21 +39,34 @@ def upload_image():
 # 2. generate ImageID
 # 3. call back-API to get image tags  
 # 4. insert new image-tags to redis
-	pass
+	img_info = request.get_data()
+	json_form = json.loads(user_info)#1
 
+ 	ImageID = Generate_ImageID()#2
+	Tags = Get_ImageTags(json_form)#3
+
+	Insert_Into_Tag_Images(json_form,Tags)#4
+	Insert_Into_Image_Tags(json_form,Tags)#4
+
+	return Tags
 @app.route('/append_tags',methods=['POST'])
 def append_tags():
 #1. receive image-tags
 #2. append coressponding tags-list
 
-	pass
+	Image_Tags_info = request.get_data()
+	ret = Append_Tags_List(Image_Tags_info)
+
+	return 'done'
 
 @app.route('/search_image',methods=['POST','GET'])
 def search_image():
 #1. receive image(decode)
 #2. call back-end API to get result sorted image list(url list)
 #3. send to client 
-	pass
+	Image = request.get_data()
+	Image_list = Search_Image(Image)
 
+	return 'image list'
 if __name__ == '__main__':
 	app.run(host=addr,port=myport)
