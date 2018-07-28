ded0f879e9dfc2e7cf7b38fd2ba1cdf7538eced8
ubuntu <ubuntu@localhost.localdomain>
2018-07-29 01:10:30 +0800

    OCR API qps limit

--- API.py
+++ API.py
@@ -2,27 +2,29 @@ import wutil
 import sys
 sys.path.append('./aip')
 from ocr import *
-
+import time
 def get_bytes_for_image(image_path):
     with open(image_path, 'rb') as image_file:
         return image_file.read()
 
 def image_path_ocr(image_path):
+    #print image_path
     appId = '11594330'
     apiKey = '6baqiIRN7EN05KrYtX5WFroM'
     secretKey = 'N65FsdDM5AsexHqdgwRyV3UIy9br8Duc'
-
+    time.sleep(0.3)
     engine = AipOcr(appId, apiKey, secretKey)
     result = engine.general(get_bytes_for_image(image_path))
+    print result
     results = []
     for wr in result['words_result']:
-        results.append(wr['words'].encode("utf-8"))
+        results.append(wr['words'].encode('utf-8'))
     return results
 
-if __name__ == '__main__':
-    results = image_path_ocr('test2.jpg')
-    for r in results:
-        print(r)
+#if __name__ == '__main__':
+#    results = image_path_ocr('test2.jpg')
+#    for r in results:
+#        print(r)
 
 
 def Get_Images_Tags(path):
