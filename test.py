673c6424b4d8940d232fcbeca211214d59524d79
ubuntu <ubuntu@localhost.localdomain>
2018-07-29 21:54:04 +0800

    test.py changes

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ url = 'http://134.175.178.44:88889'
 def test_upload_user_info():
 	path = '/upload_user_info'
 	myurl = url + path;
-	user_info = {'nickname':'wen','gender':'boy','city':'Shenzhen','provice':'Guangdong','county':'China'}
+	user_info = {'nickName':'wen','gender':'boy','city':'Shenzhen','provice':'Guangdong','country':'China'}
 	
 		
 
@@ -25,24 +25,39 @@ def test_upload_image():
 	path = '/upload_image'
 	myurl = url + path
 
-	data = {'user':'wen'}
+	data = {'nickName':'wen'}
 	image = {'image':open('./test2.jpg')}
 	
 	r = requests.post(url = myurl,data = data, files = image)
-	print r.content 
+	resp =  r.content.decode()
+	print resp
+	#Json = json.loads(resp)
+	#print Json
 
 def test_append_tags():
 	path = '/append_tags'
 	myurl = url + path
+	
+	data = {'nickName':'wen','tag':'WTF','ImageID':'test2.jpg'}	
+	r = requests.post(url = myurl,data = data)	
+	
+	print r.content.decode()
 
 def test_image_search():
 	path = '/search_image'
 	myurl  = url + path
 
+def test_tag_search():
+	path = '/tag_search'
+	myurl = url + path
 
+	data = {'nickName':'wen','tag':'hello world'}
+
+	r = requests.post(url = myurl,data = data)
 
 if __name__ == '__main__':
 	test_upload_user_info()
 	test_upload_image()
 	test_append_tags()
 	test_image_search()
+	test_tag_search()
