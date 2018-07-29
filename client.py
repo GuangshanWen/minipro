6023e943704ca044d89a9ded1451f8dc70bb91c5
ubuntu <ubuntu@localhost.localdomain>
2018-07-29 12:49:27 +0800

    client.py change

--- client.py
+++ client.py
@@ -4,13 +4,13 @@ import threading
 import time
 
 sum =0
-
+url = "http://134.175.178.44:88889"
 images = ['test2.jpg','my.jpg','hello.jpg','jietu.JPG']
-def test(i):
+def test_upload_image(i):
 	url = "http://134.175.178.44:88889/upload_image"
 	#header = {'Content-Type': 'multipart/form-data;boundary=adkjh123hk2hk'}
 	filepath="C:\\Users\\Administrator\\Desktop\\mini\\"+images[i]
-	data = {"user": "wen1"}
+	data = {"user": "guangshan"}
 	files = {"image": open(filepath, "rb")}
 	r = requests.post(url=url, data=data,files=files)
 	if r.status_code != 200:
@@ -20,8 +20,23 @@ def test(i):
 #	Json = json.loads(respo)
 #	print (Json)
 
+def test_upload_user_info():
+	path = '/upload_user_info'
+	myurl = url + path;
+	user_info = {'nickname':'guangshan','gender':'boy','city':'Shenzhen','provice':'Guangdong','county':'China'}
+	
+		
+
+	r = requests.post(url = myurl,data = user_info)
+	#req = urllib2.Request(url = myurl, data = user_info)
+	#resp_data = urllib2.urlopen(req);
+	#res = resp_data.read()
+	
+	print (r.content.decode())	
 
 if __name__ == '__main__':
-	for i in range(5):
-		t =threading.Thread(target=test,args=(i%4,))
-		t.start()
+	#for i in range(9):
+	test_upload_user_info()
+	test_upload_image(1)
+		#t =threading.Thread(target=test_upload_image,args=(i%4,))
+		#t.start()
